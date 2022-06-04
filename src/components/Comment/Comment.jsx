fd5d8d22974632bc0320558f0325f2568a3ea80e
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-06-04 10:30:08 -0300

    feat: development component avatar and application responsivity in project

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { Avatar } from '../Avatar/Avatar'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <Avatar
+                hasBorder={false}
+                src="https://github.com/VitorSSilva230.png"
+            />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Vitor Santos</strong>
+                            <time title="04 de junho de 2022" dateTime="2022-06-04 08:13:00">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>Muito bom Anderson, parabéns</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from '../Avatar/Avatar'
+import { Comment } from '../Comment/Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -5,8 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img
-                        className={styles.avatar}
+                    <Avatar
                         src="https://github.com/AndersonAslap.png"
                     />
 
@@ -43,6 +44,13 @@ export function Post() {
                     <button type="submit">Publicar</button>
                 </footer>
             </form>
+
+            <div className={styles.commentList}>
+                <Comment />
+                <Comment />
+                <Comment />
+            </div>
+
         </article>
     )
 }
