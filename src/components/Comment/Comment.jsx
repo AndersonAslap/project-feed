17f32472531b26aed5c5de3167a8551aebbab348
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-06-08 17:58:44 -0300

    feat

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,8 +1,18 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react'
 import { Avatar } from '../Avatar/Avatar'
 import styles from './Comment.module.css'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    const [likes, setLikes] = useState(0);
+
+    function handleLikeComment() {
+        setLikes((state) => {
+            return state + 1
+        });
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar
@@ -18,7 +28,7 @@ export function Comment({ content }) {
                             <time title="04 de junho de 2022" dateTime="2022-06-04 08:13:00">Cerca de 1h atrás</time>
                         </div>
 
-                        <button title="Deletar comentário">
+                        <button title="Deletar comentário" onClick={() => onDeleteComment(content)}>
                             <Trash size={24} />
                         </button>
                     </header>
@@ -26,9 +36,9 @@ export function Comment({ content }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likes}</span>
                     </button>
                 </footer>
             </div>
--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -26,8 +26,23 @@ export function Post({ author, content, publishedAt }) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('')
         setNewCommentText(event.target.value);
     }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório')
+    }
+
+    function deleteComment(commentDelete) {
+        const commentsWithoutDeleteOne = comments.filter((comment) => {
+            return comment !== commentDelete
+        })
+
+        setComments(commentsWithoutDeleteOne)
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
 
     return (
         <article className={styles.post}>
@@ -49,11 +64,11 @@ export function Post({ author, content, publishedAt }) {
             </header>
 
             <div className={styles.content}>
-                {content.map(line => {
+                {content.map((line, index) => {
                     if (line.type === 'paragraph') {
-                        return <p>{line.content}</p>
+                        return <p key={index}>{line.content}</p>
                     } else if (line.type === 'link') {
-                        return <a href="#">{line.content}</a>
+                        return <a href="#" key={index}>{line.content}</a>
                     }
                 })}
             </div>
@@ -61,19 +76,21 @@ export function Post({ author, content, publishedAt }) {
             <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
                 <textarea
+                    required
                     value={newCommentText}
                     onChange={handleNewCommentChange}
                     placeholder="Deixe um comentário"
+                    onInvalid={handleNewCommentInvalid}
                 />
 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button disabled={isNewCommentEmpty} type="submit">Publicar</button>
                 </footer>
             </form>
 
             <div className={styles.commentList}>
-                {comments.map(comment => {
-                    return <Comment content={comment} />
+                {comments.map((comment, index) => {
+                    return <Comment key={index} content={comment} onDeleteComment={deleteComment} />
                 })}
             </div>
 
