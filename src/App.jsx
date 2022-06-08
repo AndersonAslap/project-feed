ce0319842163e588e588c4c93fb2404acf05699d
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-06-07 21:02:27 -0300

    feat: add functions add comment

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css'
 
 import './global.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/AndersonAslap.png",
+      name: "Anderson Adolfo",
+      role: "Fullstack Developer"
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋" },
+      { type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: 'link', content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date('2022-06-04 23:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/VitorSSilva230.png",
+      name: "Vitor Santos",
+      role: "Fullstack Developer"
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋" },
+      { type: 'paragraph', content: "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀" },
+      { type: 'link', content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date('2022-06-03 23:37:00')
+  }
+]
+
 export function App() {
   return (
     <>
@@ -15,8 +46,15 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </>
